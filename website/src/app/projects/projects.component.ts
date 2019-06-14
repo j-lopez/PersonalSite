4ec2df1ce7471f132209e299ba7cba805050f202
j-lopez <julo4925@colorado.edu>
2019-06-13 23:55:42 -0600

    Created framework for adding projects with angular, missing actual descriptions and 'read more' button

--- website/src/app/projects/projects.component.ts
+++ website/src/app/projects/projects.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
+import { personalProjects } from './../personalProjects';
 @Component({
     selector: 'app-projects',
     templateUrl: './projects.component.html',
     styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent {
+    personalProjects = personalProjects;
+    
     constructor() { }
+
+    showLongDescription() {
+        console.log("Will figure this out soon");
+    }
 }
